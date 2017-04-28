e3522fed5cf9bb84373bce292dd3f888d30eb838
Jeffrey Olchovy <jeffo@tapad.com>
2017-04-28 15:10:22 +0200

    add unit test for library/service

--- library/src/test/scala/com/example/sbt/TweeterServiceSpec.scala
+++ library/src/test/scala/com/example/sbt/TweeterServiceSpec.scala
@@ -7,9 +7,8 @@ class TweeterServiceSpec extends FlatSpec with Matchers {
 
   behavior of "TweeterService"
 
-  it should "return a failure when ..." in {
-    pending
-    val service: TweeterService = ???
+  it should "return a failure when acting on behalf of a client with bogus credentials" in {
+    val service = TweeterService("foo", "bar", "baz", "qux")
     val result = service.post("This tweet should never make it out!")
     result shouldBe a[Failure[_]]
   }
